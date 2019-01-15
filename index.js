6f80cec3bb97d15cb5b3ebb4ae341ed1a6c29c0f
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-15 20:22:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 
-<script id="repository-template" type="text/x-handlebars-template">
-  <ul>
-   {{#each this}}
-     <li>
-       <h2><a href="{{html_url}}">{{name}}</a></h2>
-       <section>
-          <header><h4>Created By {{owner.login}}</h4></header>
-          <img src="{{owner.avatar_url}}" height="32" width="32">
-       </section>
-       <p>Watchers: {{watchers_count}}</p>
-       <p>Forks: {{forks_count}}</p>
-       <p>Issues: {{open_issues_count}}</p>
-     </li>
-    {{/each}}
-  </ul>
-</script>
 
+
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const src = document.getElementById("repository-template").innerHTML
+  const template = Handlebars.compile(src)
+  const repoList = template(repos)
+  document.getElementById("repositories").innerHTML = repoList
+}
